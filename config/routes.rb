92d4870d951e352c7f966b726c0a1e8b800742a6
Mark Murphy <mark@instadev.co>
2021-02-04 13:47:36 -0800

    Add ability for users log out

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get 'about-us', to: 'about#index', as: 'about'
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+  delete 'loggout', to: 'sessions#destroy'
 end
