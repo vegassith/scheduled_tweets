2e8662e3f9260b91920e936c6c2c0ad52209e9f7
Mark Murphy <mark@instadev.co>
2021-02-04 15:31:49 -0800

    Updated Nav Bar for Login, Signup and Sign In

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,7 @@
 class MainController < ApplicationController
   def index
     # Actions go here
-    if session[:user_id]
-      @user = User.find_by(id: session[:user_id])
-      flash.now[:notice] = 'Logged in successfully...'
-    end
+
 
 
   end
