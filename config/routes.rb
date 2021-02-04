58916dd407d56c8b8733067b7af94c842b946f62
Mark Murphy <mark@instadev.co>
2021-02-04 15:14:05 -0800

    Add ability for users to sign in

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get 'about-us', to: 'about#index', as: 'about'
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+
+    get 'sign_in', to: 'sessions#new'
+    post 'sign_in', to: 'sessions#create'
+
   delete 'loggout', to: 'sessions#destroy'
 end
